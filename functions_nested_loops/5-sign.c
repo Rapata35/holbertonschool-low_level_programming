6f646134314eac3a21948663a2bb2e0b09ec54c2
Rapata35 <9555@holbertonstudents.com>
2024-06-24 13:23:22 +0000

    tache 5 projet 3

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ *
+ * Return: 1 ifter t if n is zero, -1 if n is less than zero
+ *         Prints the sign corresponding to the return value
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
